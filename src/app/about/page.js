2ece84534803cc97765f257128117c626ae251b9
AhmedAshraf6 <ahmedp76b@gmail.com>
2023-03-12 10:33:38 +0200

    create about page

---
+++ src/app/about/page.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default function page() {
+  return (
+    <section className='max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-normal text-amber-600'> about</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+        impedit voluptates perferendis dignissimos delectus, accusantium maiores
+        consequuntur ab, reprehenderit aliquid fugit pariatur dolorum vero magni
+        reiciendis recusandae. Perferendis, officiis error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
+        sint doloribus tempore aperiam, adipisci laboriosam ad voluptates magnam
+        commodi veritatis quibusdam, animi dignissimos dolorem beatae asperiores
+        porro omnis doloremque neque vel distinctio? Iste, architecto vero?
+        Velit, libero. Corrupti labore unde repellat itaque! A quidem deserunt
+        maiores necessitatibus quisquam illum iusto.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
+        eveniet, asperiores illum similique repellendus reiciendis suscipit
+        eaque, est accusamus consequuntur error, fuga neque? Quaerat quae
+        provident cumque odit repellat aliquid repudiandae dolorem! Quo,
+        consequatur corrupti, atque, a est tempora neque aut velit dolorum vero
+        alias perferendis fugiat? Totam, suscipit? Ratione, reiciendis culpa,
+        eos ipsam nihil neque inventore corporis enim porro molestiae itaque
+        distinctio facere id nemo illo minus error quas repellendus eligendi
+        accusamus. Corporis nulla blanditiis fugiat laboriosam, reprehenderit
+        obcaecati.
+      </p>
+    </section>
+  );
+}
